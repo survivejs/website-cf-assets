eb1fa5ace386414c0f763ae7c63a674a2d73e0ca
Juho Vepsäläinen <bebraw@gmail.com>
2022-01-19 16:34:10 +0200

    chore: Add notes

--- workers/get-images.ts
+++ workers/get-images.ts
@@ -1,5 +1,11 @@
 import { getImages } from "../src/getImages.ts";
 
+// A good optimization here would be to write images to a KV store on upload
+// and then read the images from a KV store here. Reading from the Cloudflare
+// API comes with some lag due to multiple requests but for small amounts of
+// images that's acceptable.
+//
+// Another good addition would be securing the end point with an auth key.
 export default {
   async fetch(_request: Request, env: { accountId: string; apiToken: string }) {
     try {
