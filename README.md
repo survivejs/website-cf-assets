2f035bc1563c5e0a1809e21b5da2bb5db1f0ad41
Juho Vepsäläinen <bebraw@gmail.com>
2022-01-19 13:45:15 +0200

    feat: Set up an upload script

--- README.md
+++ README.md
@@ -2,3 +2,25 @@
 
 These assets are cached on Cloudflare for resizing etc.
 
+## Uploading images
+
+Before uploading, set up `config.json` like below:
+
+**config.json**
+
+```json
+{
+  "account": "todo",
+  "token": "todo"
+}
+```
+
+You can get the account id through CloudFlare UI. [See this blog post to understand how to generate the token](https://www.better.dev/cloudflare-images).
+
+It is important you don't commit this file to the repository for security reasons.
+
+To upload the images, run `deno run -A ./mod.ts`.
+
+## Reference
+
+* [Documentation](https://developers.cloudflare.com/images/)
---
+++ mod.ts
@@ -0,0 +1,44 @@
+import config from "./config.json" assert { type: "json" };
+
+import * as path from "https://deno.land/std@0.113.0/path/mod.ts";
+
+const files = (await dir("img", [".jpg", ".png"])).map((o) => o.path);
+
+for (const file of files) {
+  console.log("uploading", file);
+  await upload(config.account, config.token, file);
+  console.log("uploaded", file);
+}
+
+async function upload(accountId: string, token: string, p: string) {
+  const form = new FormData();
+
+  form.append("file", new Blob([await Deno.readFile(p)]), p);
+
+  return fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${accountId}/images/v1`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: form,
+    },
+  );
+}
+
+async function dir(p: string, extensions?: string[]) {
+  const ret = [];
+
+  for await (const { name } of Deno.readDir(p)) {
+    if (extensions) {
+      if (extensions.includes(path.extname(name))) {
+        ret.push({ path: path.join(p, name), name });
+      }
+    } else {
+      ret.push({ path: path.join(p, name), name });
+    }
+  }
+
+  return ret;
+}
